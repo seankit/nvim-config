e2a8755896686ac93ec013b4af39c14d670fc3fa
Sean Kelly <sean.kelly@grindr.com>
2024-12-28 23:56:09 -0800

    working

---
+++ lua/config/options.lua
@@ -0,0 +1,23 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.g.lazyvim_picker = "telescope"
+vim.opt.termguicolors = false
+vim.opt.guicursor = ""
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+
+vim.opt.incsearch = true
+vim.opt.scrolloff = 2
+vim.opt.signcolumn = "yes"
+
+vim.opt.updatetime = 50
